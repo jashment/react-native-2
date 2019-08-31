9e7b627fed9e6cda2d68408bb432b95e7514da11
Jordan <jordanashment@gmail.com>
2019-08-31 16:37:30 -0600

    Creating Components

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,11 @@ import {View, Text, StyleSheet} from 'react-native'
 
 const Header = props => {
     return (
-    <View style={styles.header}>
-        <Text style={styles.headerTitle}>{props.title}</Text>
-    </View>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
+        </View>
     )
+
 };
 
 const styles = StyleSheet.create({
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+
+import Card from '../components/Card'
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>Start A New Game</Text>
+            <Card style={styles.inputContainer}>
+                <Text style={styles.title}>Select A Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Reset" onPress={() => {}} color="#c717fc" />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Confirm" onPress={() => {}} color="#f7287b" />
+                    </View>
+                </View>
+            </Card>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+
+    },
+    button: {
+        width: 100,
+    }
+});
+
+export default StartGameScreen
